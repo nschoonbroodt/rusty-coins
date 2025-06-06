b3704cdf28706bb43cf1936f18cc7b7bb8c5227d
Nicolas Schoonbroodt <nicolas@elsewhere.be>
2025-06-06 13:19:23 +0200

    adding AccountName newtype

--- crates/coins-core/src/error.rs
+++ crates/coins-core/src/error.rs
@@ -2,6 +2,11 @@
 pub enum CoinsCoreError {
     #[error("Commodity with symbol '{0}' already exists")]
     CommodityAlreadyExists(String),
+    #[error(
+        "Invalid account name: {0}. Account names must starts with 'Assets:', 'Liabilities:', 'Income:', 'Expenses:', or 'Equity:'."
+    )]
+    InvalidAccountName(String),
+
     #[error(transparent)]
     Rusqlite(#[from] rusqlite::Error),
     #[error(transparent)]
--- crates/coins-core/src/model/account.rs
+++ crates/coins-core/src/model/account.rs
@@ -1,24 +1,47 @@
+use core::fmt;
+
 use crate::prelude::*;
 use bon::bon;
 
 #[derive(Debug)]
+pub struct AccountName(String);
+impl AccountName {
+    pub fn new(name: &str) -> Result<Self> {
+        if !name.starts_with("Assets:")
+            && !name.starts_with("Liabilities:")
+            && !name.starts_with("Revenue:")
+            && !name.starts_with("Expenses:")
+            && !name.starts_with("Equity:")
+        {
+            return Err(CoinsCoreError::InvalidAccountName(name.to_string()));
+        }
+        Ok(Self(name.to_string()))
+    }
+}
+impl fmt::Display for AccountName {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Debug)]
 pub struct Account {
     id: i64,
-    name: String,
+    name: AccountName,
 }
 
 #[bon]
 impl Account {
     #[builder]
-    pub fn new(#[builder(start_fn)] model: &super::CoinsModel, name: String) -> Result<Self> {
+    pub fn new(#[builder(start_fn)] model: &super::CoinsModel, name: AccountName) -> Result<Self> {
         let mut stmt = model
             .conn
             .prepare("INSERT INTO accounts (name) VALUES (?1) RETURNING id, name")?;
-        let mut account_rows = stmt.query(rusqlite::params![name])?;
+        let mut account_rows = stmt.query(rusqlite::params![name.0])?;
         let row = account_rows.next()?.unwrap();
         Ok(Self {
             id: row.get(0)?,
-            name: row.get(1)?,
+            name: AccountName(row.get(1)?),
         })
     }
 
@@ -38,7 +61,7 @@ impl Account {
         if let Some(row) = rows.next()? {
             Ok(Some(Self {
                 id: row.get(0)?,
-                name: row.get(1)?,
+                name: AccountName(row.get(1)?),
             }))
         } else {
             Ok(None)
@@ -48,7 +71,7 @@ impl Account {
     pub fn id(&self) -> i64 {
         self.id
     }
-    pub fn name(&self) -> &str {
+    pub fn name(&self) -> &AccountName {
         &self.name
     }
 
@@ -58,7 +81,7 @@ impl Account {
             .query_map([], |row| {
                 Ok(Account {
                     id: row.get(0)?,
-                    name: row.get(1)?,
+                    name: AccountName(row.get(1)?),
                 })
             })?
             .flatten()
@@ -77,6 +100,7 @@ mod tests {
     fn test_accounts() {
         let model = CoinsModel::new(None).unwrap();
         let conn = &model.conn;
+        let accounts_pre = Account::all(&model).unwrap();
 
         conn.execute(r#"INSERT INTO accounts (name) VALUES ("Account1")"#, ())
             .unwrap();
@@ -84,15 +108,15 @@ mod tests {
         println!("{}", pretty_sqlite::pretty_table(conn, "accounts").unwrap());
 
         let accounts = Account::all(&model).unwrap();
-        assert_eq!(accounts.len(), 1);
-        assert_eq!(accounts[0].name(), "Account1");
+        assert_eq!(accounts.len(), accounts_pre.len() + 1);
+        assert!(accounts.iter().any(|a| a.name().0 == "Account1"));
     }
 
     #[test]
     fn test_create_account() {
         let model = CoinsModel::new(None).unwrap();
         let account = Account::builder(&model)
-            .name("Test Account".to_string())
+            .name(AccountName::new("Assets:Test Account").unwrap())
             .build()
             .unwrap();
 
@@ -101,7 +125,7 @@ mod tests {
             pretty_sqlite::pretty_table(&model.conn, "accounts").unwrap()
         );
 
-        assert_eq!(account.name(), "Test Account");
+        assert_eq!(account.name().0, "Assets:Test Account");
         assert!(account.id() > 0);
 
         let accounts = Account::all(&model).unwrap();
@@ -112,7 +136,7 @@ mod tests {
     fn test_delete_account() {
         let model = CoinsModel::new(None).unwrap();
         let account = Account::builder(&model)
-            .name("Account to Delete".to_string())
+            .name(AccountName::new("Assets::Account to Delete").unwrap())
             .build()
             .unwrap();
 
@@ -133,7 +157,7 @@ mod tests {
     fn test_get_account_by_id() {
         let model = CoinsModel::new(None).unwrap();
         let account = Account::builder(&model)
-            .name("Account to Retrieve".to_string())
+            .name(AccountName::new("Account to Retrieve").unwrap())
             .build()
             .unwrap();
 
@@ -144,6 +168,6 @@ mod tests {
 
         let retrieved_account = Account::by_id(&model, account.id()).unwrap();
         assert!(retrieved_account.is_some());
-        assert_eq!(retrieved_account.unwrap().name(), "Account to Retrieve");
+        assert_eq!(retrieved_account.unwrap().name().0, "Account to Retrieve");
     }
 }
